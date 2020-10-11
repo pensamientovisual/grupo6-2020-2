37b5982ee1d588758967231050525412db4e9716
catalina.arcila <catalina.arcila@uc.cl>
2020-10-11 17:30:46 -0300

    con el grafico

--- javascript/java.js
+++ javascript/java.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     $("#Metropolitana").click(function(){
         var x = document.getElementById("DotMatrixChart");
+        var y = document.getElementById("cuadro")
         if (x.style.visibility === 'hidden') {
             x.style.visibility = 'visible';
+            y.style.visibility = "hidden";
         } else {
             x.style.visibility = 'hidden';
         }  	
